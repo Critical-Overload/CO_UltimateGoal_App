4477af3b047336f1398a84c9196e001e4146c67d
Adrian Chan <adrian27513@gmail.com>
2019-09-14 17:40:20 -0400

    Renamed old object detection Copied Tensor Flow Sky Stone Detection into folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptMineralObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptMineralObjectDetection.java
@@ -53,7 +53,7 @@ import java.util.List;
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
 
-public class ConceptTensorFlowObjectDetection extends LinearOpMode {
+public class ConceptMineralObjectDetection extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
     private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
     private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
