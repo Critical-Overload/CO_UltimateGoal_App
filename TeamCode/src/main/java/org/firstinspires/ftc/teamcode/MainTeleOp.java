b3c7d97ceeee4ce7dba7aefef5c6fba135a377d6
CriticalOverload <critical.overlord@gmail.com>
2019-12-14 19:09:09 -0500

    Add new intake and arm prototype (untested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -12,12 +13,14 @@ public class MainTeleOp extends LinearOpMode {
     private DcMotor motorFrontLeft;
     private DcMotor motorBackLeft;
     private DcMotor motorBackRight;
-    private DcMotor leftIntake;
-    private DcMotor rightIntake;
     private DcMotor arm;
+    private CRServo leftIntake;
+    private CRServo rightIntake;
 
     private Servo leftIntakeServo;
     private Servo rightIntakeServo;
+    private Servo flimsy;
+    private Servo clawMover;
     private Servo claw;
 
     @Override
@@ -27,31 +30,31 @@ public class MainTeleOp extends LinearOpMode {
         motorFrontLeft = hardwareMap.dcMotor.get("FL");
         motorBackLeft = hardwareMap.dcMotor.get("BL");
         motorBackRight = hardwareMap.dcMotor.get("BR");
-        leftIntake = hardwareMap.dcMotor.get("LI");
-        rightIntake = hardwareMap.dcMotor.get("RI");
         arm = hardwareMap.dcMotor.get("arm");
+
+        leftIntake = hardwareMap.crservo.get("LI");
+        rightIntake = hardwareMap.crservo.get("RI");
 
         leftIntakeServo = hardwareMap.servo.get("LIservo");
         rightIntakeServo = hardwareMap.servo.get("RIservo");
+        flimsy = hardwareMap.servo.get("flimsy");
+        clawMover = hardwareMap.servo.get("clawMover");
         claw = hardwareMap.servo.get("claw");
 
         motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
         motorBackRight.setDirection(DcMotor.Direction.REVERSE);
-        rightIntake.setDirection(DcMotor.Direction.REVERSE);
+        leftIntake.setDirection(CRServo.Direction.REVERSE);
 
         motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         double powerMod = 1.0;
         double intakeMod = 1.0;
 
         waitForStart();
-
-        claw.setPosition(0);
 
         while(opModeIsActive()){
 
@@ -64,24 +67,62 @@ public class MainTeleOp extends LinearOpMode {
                 powerMod = 1.0;
             }
 
-            if(gamepad1.left_bumper){
-                intakeMod = -1.0;
+            if(gamepad2.left_bumper){
+                intakeMod = 0.5;
             }else{
                 intakeMod = 1.0;
             }
 
             if(gamepad2.a){
+                leftIntakeServo.setPosition(0);
+                rightIntakeServo.setPosition(1);
+            }
+
+            if(gamepad2.b){
                 leftIntakeServo.setPosition(1);
                 rightIntakeServo.setPosition(0);
             }
 
-            arm.setPower(gamepad2.right_stick_y);
+            if(gamepad2.dpad_up){
+                leftIntakeServo.setPosition(leftIntakeServo.getPosition() + 0.01);
+                rightIntakeServo.setPosition(rightIntakeServo.getPosition() - 0.01);
 
-            if(gamepad2.b){
-                claw.setPosition(1);
-            }else if(gamepad2.x){
+            }
+
+            if(gamepad2.dpad_down){
+                leftIntakeServo.setPosition(leftIntakeServo.getPosition() - 0.01);
+                rightIntakeServo.setPosition(rightIntakeServo.getPosition() + 0.01);
+            }
+
+            if(gamepad2.x){
                 claw.setPosition(0);
             }
+
+            if(gamepad2.y){
+                claw.setPosition(1);
+            }
+
+            arm.setPower(gamepad2.right_stick_y);
+
+            if(gamepad2.dpad_left){
+                clawMover.setPosition(clawMover.getPosition() - 0.01);
+            }
+
+            if(gamepad2.dpad_right){
+                clawMover.setPosition(clawMover.getPosition() + 0.01);
+            }
+
+            telemetry.addData("Claw mover position", clawMover.getPosition());
+
+            if(gamepad1.a){
+                flimsy.setPosition(flimsy.getPosition() + 0.01);
+            }
+
+            if(gamepad1.b){
+                flimsy.setPosition(flimsy.getPosition() - 0.01);
+            }
+
+            telemetry.addData("Flimsy servo position", flimsy.getPosition());
 
             leftIntake.setPower((gamepad2.right_trigger * intakeMod)-(gamepad2.left_trigger * intakeMod));
             rightIntake.setPower((gamepad2.right_trigger * intakeMod)-(gamepad2.left_trigger * intakeMod));
@@ -102,6 +143,8 @@ public class MainTeleOp extends LinearOpMode {
             telemetry.addData("BL Power", motorBackLeft.getPower());
             telemetry.addData("FR Power", motorFrontRight.getPower());
             telemetry.addData("BR Power", motorBackRight.getPower());
+
+            telemetry.update();
             idle();
         }
     }
